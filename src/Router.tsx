c76e6edbcb947d40fd8d2c9c208d30922aa9b8a7
alexgoni <goni000211@gmail.com>
2025-06-12 15:24:07 +0900

    chore: 폴더 구조 및 절대 경로 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import LandingPage from "./pages/landing";
+import LandingPage from "@/pages/landing";
 
 export default function Router() {
   return (
