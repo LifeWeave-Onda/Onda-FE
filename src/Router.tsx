94ddd502035103c4129693396c2e1f5bbadf3672
daeun <rlaekdms901@naver.com>
2025-06-25 12:07:56 +0900

    feat: 아이디/비밀번호 찾기 페이지 라우팅 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import LandingPage from "@/pages/landing";
 
+import FindPage from "./pages/find-id-password";
 import LoginPage from "./pages/login";
 
 export default function Router() {
@@ -9,7 +10,8 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/find" element={<FindPage />} />
       </Routes>
     </BrowserRouter>
   );
