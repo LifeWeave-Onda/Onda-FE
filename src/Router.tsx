4ddc1c3b45057b5d7150a46948fcdee736f783cc
daeun <rlaekdms901@naver.com>
2025-07-06 17:58:58 +0900

    feat: 아이디/비밀번호 찾기 결과 페이지 라우팅 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,8 @@ import BreederListPage from "@/pages/breeder-list";
 import LandingPage from "@/pages/landing";
 
 import FindIdPasswordPage from "./pages/find-id-password";
+import FindIdResult from "./pages/find-id-password/result/FindIdResult";
+import FindPasswordResult from "./pages/find-id-password/result/FindPasswordResult";
 import LoginPage from "./pages/login";
 
 export default function Router() {
@@ -13,6 +15,14 @@ export default function Router() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/find-id-password" element={<FindIdPasswordPage />} />
+        <Route
+          path="/find-id-password/find-id-result"
+          element={<FindIdResult />}
+        />
+        <Route
+          path="/find-id-password/find-password-result"
+          element={<FindPasswordResult />}
+        />
         <Route path="/breeder-list" element={<BreederListPage />} />
       </Routes>
     </BrowserRouter>
