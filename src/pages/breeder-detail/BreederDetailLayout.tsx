92034d12bf7f1ddd2b55b9888f3b044a73c18b39
alexgoni <goni000211@gmail.com>
2025-07-07 13:52:19 +0900

    feat: svgr

--- src/pages/breeder-detail/BreederDetailLayout.tsx
+++ src/pages/breeder-detail/BreederDetailLayout.tsx
@@ -1,9 +1,12 @@
 import { css } from "@emotion/react";
-import { NavLink, Outlet } from "react-router-dom";
+import { useState } from "react";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 
 import addressIcon from "@/assets/breeder-detail/address.svg";
 import emailIcon from "@/assets/breeder-detail/email.svg";
+import HeartIcon from "@/assets/breeder-detail/heart.svg?react";
 import phoneIcon from "@/assets/breeder-detail/phone.svg";
+import animalProfileImg from "@/assets/breeder-detail/temp/animal-profile.png";
 import breederProfileImg from "@/assets/breeder-detail/temp/breeder-profile.png";
 import MainLayout from "@/components/layout/MainLayout";
 import { breakpoints, responsiveHorizontalPadding } from "@/styles/layout";
@@ -18,27 +21,7 @@ export default function BreederDetailLayout() {
   return (
     <MainLayout>
       <div css={layoutStyle}>
-        <div className="left-content">
-          <h1 className="title">노일호특수목적견</h1>
-          <img src={breederProfileImg} alt="profile" className="profile-img" />
-          <div css={contactInfoStyle}>
-            <div className="contact-title">연락처 정보</div>
-            <div className="contact-flexbox">
-              <div className="row">
-                <img src={addressIcon} alt="address" width={20} height={20} />
-                <span>{CONTACT_INFO_DATA.address}</span>
-              </div>
-              <div className="row">
-                <img src={phoneIcon} alt="phone" width={20} height={20} />
-                <span>{CONTACT_INFO_DATA.phone}</span>
-              </div>
-              <div className="row">
-                <img src={emailIcon} alt="email" width={20} height={20} />
-                <span>{CONTACT_INFO_DATA.email}</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <LeftSidebar />
 
         <div className="right-content">
           <div css={tabStyle}>
@@ -56,10 +39,58 @@ export default function BreederDetailLayout() {
   );
 }
 
+function LeftSidebar() {
+  const location = useLocation();
+  const isBreederInfoPage = location.pathname.endsWith("/info");
+  const isAnimalPage = location.pathname.endsWith("/animal");
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  return (
+    <div css={leftSidebarStyle}>
+      <h1 className="title">
+        {isBreederInfoPage && <>노일호특수목적견</>}
+        {isAnimalPage && <>보미</>}
+      </h1>
+      <div style={{ position: "relative" }}>
+        <img
+          src={isBreederInfoPage ? breederProfileImg : animalProfileImg}
+          alt="profile"
+          className="profile-img"
+        />
+        <button
+          type="button"
+          className="heart-btn"
+          onClick={() => setIsLiked((prev) => !prev)}
+        >
+          <HeartIcon fill={!isLiked ? "white" : "#FF4D4F"} />
+        </button>
+      </div>
+
+      <div css={contactInfoStyle}>
+        <div className="contact-title">연락처 정보</div>
+        <div className="contact-flexbox">
+          <div className="row">
+            <img src={addressIcon} alt="address" width={20} height={20} />
+            <span>{CONTACT_INFO_DATA.address}</span>
+          </div>
+          <div className="row">
+            <img src={phoneIcon} alt="phone" width={20} height={20} />
+            <span>{CONTACT_INFO_DATA.phone}</span>
+          </div>
+          <div className="row">
+            <img src={emailIcon} alt="email" width={20} height={20} />
+            <span>{CONTACT_INFO_DATA.email}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 const layoutStyle = css`
   ${responsiveHorizontalPadding}
-  padding-left: 20px;
-  padding-right: 20px;
+  padding-bottom: 240px;
   margin-top: 36px;
   display: flex;
   gap: 20px;
@@ -68,25 +99,39 @@ const layoutStyle = css`
     gap: 36px;
   }
 
-  .left-content {
-    flex: 1;
-    max-width: 500px;
-
-    .title {
-      font-weight: 700;
-      font-size: 24px;
-      color: #222222;
-    }
-
-    .profile-img {
-      width: 100%;
-      aspect-ratio: 1 / 1;
-      margin-top: 8px;
-    }
-  }
-
   .right-content {
     flex: 1.3;
+  }
+`;
+
+const leftSidebarStyle = css`
+  flex: 1;
+  max-width: 500px;
+
+  .title {
+    font-weight: 700;
+    font-size: 24px;
+    color: #222222;
+  }
+
+  .profile-img {
+    width: 100%;
+    aspect-ratio: 1 / 1;
+    margin-top: 8px;
+  }
+
+  .heart-btn {
+    position: absolute;
+    right: 24px;
+    bottom: 24px;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    background: #ffffff66;
+    backdrop-filter: blur(10px);
   }
 `;
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
-
-import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
   },
