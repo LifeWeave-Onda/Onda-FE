18f0bfc2f494469636585ac201f15a18ad38b729
alexgoni <goni000211@gmail.com>
2025-07-07 18:20:20 +0900

    fix: build error

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -1,4 +1,5 @@
-declare module "*.svg" {
-  const content: React.FC<React.SVGProps<SVGElement>>;
+declare module "*.svg?react" {
+  import * as React from "react";
+  const content: React.FC<React.SVGProps<SVGSVGElement>>;
   export default content;
 }
